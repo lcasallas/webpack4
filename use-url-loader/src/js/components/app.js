4ce869f3a2b2d2f3e94cd4b97b00216ef31a39b1
Leonardo Beltran <lcbeltran@eis.com.co>
2020-02-20 19:10:27 -0500

    agregando preprocesadores y archivos multimedia a webapack

---
+++ use-url-loader/src/js/components/app.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import data from './data.json';
+import Loader from './loader';
+import logo from '../../images/platzi.png';
+import video from '../../video/que-es-core.mp4';
+
+console.log(data);
+
+function App() {
+	const [loaderList, setLoaderList] = useState([]);
+
+	function handleClick() {
+		setLoaderList(data.loaders);
+	}
+
+	return (
+		<div>
+			<p>
+				<img src={logo} width={40} />
+				<video src={video} width={360} controls poster={logo}></video>
+			</p>
+			<ul>
+				{loaderList.map(item => (
+					<Loader {...item} key={item.id} />
+				))}
+			</ul>
+			<button onClick={handleClick}>Mostrar lo aprendido</button>
+		</div>
+	);
+}
+
+export default App;
---
+++ use-url-loader/src/js/text.js
@@ -0,0 +1,5 @@
+function text() {
+	document.body.innerHTML = 'hola mundo webpack 200 @LeCode ;) -> check';
+}
+
+export default text;
